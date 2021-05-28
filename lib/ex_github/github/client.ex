5f2970906ec1dda34458f721a477e2ce1dfb1900
LP <luizpaulobborges@gmail.com>
2021-05-28 16:31:18 -0300

    added github client module

---
+++ lib/ex_github/github/client.ex
@@ -0,0 +1,40 @@
+defmodule ExGithub.Github.Client do
+  use Tesla
+
+  alias ExGithub.Error
+  alias Tesla.Env
+
+  @base_url "https://api.github.com/users/"
+  plug Tesla.Middleware.Headers, [{"user-agent", "Tesla"}]
+  plug Tesla.Middleware.JSON
+
+  def get_user_repos(url \\ @base_url, username) do
+    "#{url}#{username}/repos?per_page=2"
+    |> get()
+    |> handle_get()
+  end
+
+  defp handle_get({:ok, %Env{status: 200, body: body}}) do
+    repos = Enum.map(body, fn element -> handle_repo(element) end)
+
+    {:ok, repos}
+  end
+
+  defp handle_get({:ok, %Env{status: 404, body: _body}}) do
+    {:error, Error.build(:not_found, "Not found")}
+  end
+
+  defp handle_repo(%{
+         "id" => id,
+         "name" => name,
+         "html_url" => html_url,
+         "stargazers_count" => stargazers_count
+       }) do
+    %{
+      "id" => id,
+      "name" => name,
+      "html_url" => html_url,
+      "stargazers_count" => stargazers_count
+    }
+  end
+end
