1067e1b1dff8c64a0c888da45a46e6b84d10eb17
LP <luizpaulobborges@gmail.com>
2021-05-28 15:18:02 -0300

    added tesla

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule ExGithub.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.17.0"}
     ]
   end
 
